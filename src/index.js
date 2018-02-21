a889f6d02499b2091b87fd31fb548a49f236b469
Eduard Nikolenko <nikolenko.eduard@gmail.com>
2018-02-21 16:38:21 +0300

    First build

--- src/index.js
+++ src/index.js
@@ -1,20 +1,30 @@
+function padStart (input, length, symbol = ' ') {
+  let str = input.toString()
+
+  while (str.length < length) {
+    str = symbol + str
+  }
+
+  return str
+}
+
 function dateFormat (date = new Date(), format = 'YYYY.MM.DD HH:mm:ss') {
   return format
     .replace('YYYY', date.getFullYear())
     .replace('YY', date.getFullYear() % 100)
-    .replace('MM', `${date.getMonth() + 1}`.padStart(2, '0'))
+    .replace('MM', padStart(date.getMonth() + 1, 2, '0'))
     .replace('M', date.getMonth() + 1)
-    .replace('DD', `${date.getDate()}`.padStart(2, '0'))
+    .replace('DD', padStart(date.getDate(), 2, '0'))
     .replace('D', date.getDate())
-    .replace('HH', `${date.getHours()}`.padStart(2, '0'))
+    .replace('HH', padStart(date.getHours(), 2, '0'))
     .replace('H', date.getHours())
-    .replace('hh', `${date.getHours() % 12}`.padStart(2, '0'))
+    .replace('hh', padStart(date.getHours() % 12, 2, '0'))
     .replace('h', date.getHours() % 12)
     .replace('A', date.getHours() < 12 ? 'AM' : 'PM')
     .replace('a', date.getHours() < 12 ? 'am' : 'pm')
-    .replace('mm', `${date.getMinutes()}`.padStart(2, '0'))
+    .replace('mm', padStart(date.getMinutes(), 2, '0'))
     .replace('m', date.getMinutes())
-    .replace('ss', `${date.getSeconds()}`.padStart(2, '0'))
+    .replace('ss', padStart(date.getSeconds(), 2, '0'))
     .replace('s', date.getSeconds())
 }
 
