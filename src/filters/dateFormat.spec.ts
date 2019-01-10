d37b70bdc98d9038b2f844c9d1d5c80963e418bc
Eduard Nikolenko <info@eduardnikolenko.com>
2019-01-10 03:23:27 +0300

    Setup tests

---
+++ src/filters/dateFormat.spec.ts
@@ -0,0 +1,30 @@
+import ava, { TestInterface } from 'ava';
+import { dateFormatFilter } from './dateFormat';
+
+const test = ava as TestInterface<{ date?: Date }>;
+
+const macro = (t, input: Date, format: string, expected: string) => {
+  t.is(dateFormatFilter(input, format), expected);
+};
+
+const dateAm = new Date(2018, 11, 1, 10, 37, 23);
+
+const datePm = new Date(2018, 11, 1, 13, 37, 23);
+
+test('DD.MM.YYYY', macro, dateAm, 'DD.MM.YYYY', '01.12.2018');
+
+test('YYYY.MM.DD', macro, dateAm, 'YYYY.MM.DD', '2018.12.01');
+
+test('YYYY.MM.DD HH.mm.ss', macro, dateAm, 'YYYY.MM.DD HH.mm.ss', '2018.12.01 10.37.23');
+
+test('YYYY.MM.DD hh.mm.ss A (AM)', macro, dateAm, 'YYYY.MM.DD hh.mm.ss A', '2018.12.01 10.37.23 AM');
+
+test('YYYY.MM.DD hh.mm.ss A (PM)', macro, datePm, 'YYYY.MM.DD hh.mm.ss A', '2018.12.01 01.37.23 PM');
+
+test('YYYY.MM.DD hh.mm.ss a (AM)', macro, dateAm, 'YYYY.MM.DD hh.mm.ss a', '2018.12.01 10.37.23 am');
+
+test('YYYY.MM.DD hh.mm.ss a (PM)', macro, datePm, 'YYYY.MM.DD hh.mm.ss a', '2018.12.01 01.37.23 pm');
+
+test('MM/DD/YYYY', macro, dateAm, 'MM/DD/YYYY', '12/01/2018');
+
+test('YYYY-MM-DD', macro, dateAm, 'YYYY-MM-DD', '2018-12-01');
--- src/filters/dateFormat.ts
+++ src/filters/dateFormat.ts
@@ -1,19 +1,18 @@
-export const dateFormatFilter = (date: Date = new Date(), format: string = 'YYYY.MM.DD HH:mm:ss'): string => {
-  return format
-    .replace('YYYY', `${date.getFullYear()}`)
-    .replace('YY', `${date.getFullYear() % 100}`)
-    .replace('MM', `${date.getMonth() + 1}`.padStart(2, '0'))
-    .replace('M', `${date.getMonth() + 1}`)
-    .replace('DD', `${date.getDate()}`.padStart(2, '0'))
-    .replace('D', `${date.getDate()}`)
-    .replace('HH', `${date.getHours()}`.padStart(2, '0'))
-    .replace('H', `${date.getHours()}`)
-    .replace('hh', `${date.getHours() % 12}`.padStart(2, '0'))
-    .replace('h', `${date.getHours() % 12}`)
-    .replace('A', date.getHours() < 12 ? 'AM' : 'PM')
-    .replace('a', date.getHours() < 12 ? 'am' : 'pm')
-    .replace('mm', `${date.getMinutes()}`.padStart(2, '0'))
-    .replace('m', `${date.getMinutes()}`)
-    .replace('ss', `${date.getSeconds()}`.padStart(2, '0'))
-    .replace('s', `${date.getSeconds()}`);
-};
+export const dateFormatFilter = (date: Date = new Date(), format: string = 'YYYY.MM.DD HH:mm:ss'): string => format
+  .replace('YYYY', `${date.getFullYear()}`)
+  .replace('YY', `${date.getFullYear() % 100}`)
+  .replace('MM', `${date.getMonth() + 1}`.padStart(2, '0'))
+  .replace('M', `${date.getMonth() + 1}`)
+  .replace('DD', `${date.getDate()}`.padStart(2, '0'))
+  .replace('D', `${date.getDate()}`)
+  .replace('HH', `${date.getHours()}`.padStart(2, '0'))
+  .replace('H', `${date.getHours()}`)
+  .replace('hh', `${date.getHours() % 12}`.padStart(2, '0'))
+  .replace('h', `${date.getHours() % 12}`)
+  .replace('mm', `${date.getMinutes()}`.padStart(2, '0'))
+  .replace('m', `${date.getMinutes()}`)
+  .replace('ss', `${date.getSeconds()}`.padStart(2, '0'))
+  .replace('s', `${date.getSeconds()}`)
+  // Always last
+  .replace('A', date.getHours() < 12 ? 'AM' : 'PM')
+  .replace('a', date.getHours() < 12 ? 'am' : 'pm');
