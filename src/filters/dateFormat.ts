f810aef25d7fdde0839b7a4040eaa6427951c744
Eduard Nikolenko <info@eduardnikolenko.com>
2019-03-20 22:51:01 +0300

    Fix padStart, new version, closes #7

--- src/filters/dateFormat.ts
+++ src/filters/dateFormat.ts
@@ -3,6 +3,10 @@ export interface IDateFormatConfig {
   monthNamesShort: string[];
 }
 
+const padStart = (input: string, maxLength: number = 0, fillString: string = ' '): string => {
+  return `${[...new Array(maxLength - input.length)].map(() => fillString).join('')}${input}`;
+};
+
 const defaultConfig: IDateFormatConfig = {
   monthNames: [
     'January', 'February', 'March', 'April', 'May', 'June',
@@ -26,17 +30,17 @@ export const dateFormat = (
     .replace('YY', `${date.getFullYear() % 100}`)
     .replace('MMMM', `${config.monthNames[date.getMonth()]}`)
     .replace('MMM', `${config.monthNamesShort[date.getMonth()]}`)
-    .replace('MM', `${date.getMonth() + 1}`.padStart(2, '0'))
+    .replace('MM', padStart(`${date.getMonth() + 1}`, 2, '0'))
     .replace('M', `${date.getMonth() + 1}`)
-    .replace('DD', `${date.getDate()}`.padStart(2, '0'))
+    .replace('DD', padStart(`${date.getDate()}`, 2, '0'))
     .replace('D', `${date.getDate()}`)
-    .replace('HH', `${date.getHours()}`.padStart(2, '0'))
+    .replace('HH', padStart(`${date.getHours()}`, 2, '0'))
     .replace('H', `${date.getHours()}`)
-    .replace('hh', `${date.getHours() % 12}`.padStart(2, '0'))
+    .replace('hh', padStart(`${date.getHours() % 12}`, 2, '0'))
     .replace('h', `${date.getHours() % 12}`)
-    .replace('mm', `${date.getMinutes()}`.padStart(2, '0'))
+    .replace('mm', padStart(`${date.getMinutes()}`, 2, '0'))
     .replace('m', `${date.getMinutes()}`)
-    .replace('ss', `${date.getSeconds()}`.padStart(2, '0'))
+    .replace('ss', padStart(`${date.getSeconds()}`, 2, '0'))
     .replace('s', `${date.getSeconds()}`)
     .replace('A', date.getHours() < 12 ? 'AM' : 'PM')
     .replace('a', date.getHours() < 12 ? 'am' : 'pm');
