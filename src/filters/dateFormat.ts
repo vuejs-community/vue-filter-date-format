045dba4b6af8303356947268f6b4b6e53a51ddc4
Eduard Nikolenko <eduardnikolenko@users.noreply.github.com>
2019-04-05 02:36:32 +0300

    Use token onetime, closes #9

--- src/filters/dateFormat.ts
+++ src/filters/dateFormat.ts
@@ -3,10 +3,8 @@ export interface IDateFormatConfig {
   monthNamesShort: string[];
 }
 
-const padStart = (input: string, maxLength: number = 0, fillString: string = ' '): string => {
-  const length = maxLength - input.length;
-
-  return `${Array.apply(null, { length }).map(() => fillString).join('')}${input}`;
+const padZeros = (input: number, maxLength: number = 0): string => {
+  return `0000${input}`.slice(-maxLength);
 };
 
 const defaultConfig: IDateFormatConfig = {
@@ -28,22 +26,40 @@ export const dateFormat = (
   config = { ...defaultConfig, ...config };
 
   return format
-    .replace('YYYY', `${("0000" + date.getFullYear()).slice(-4)}`)
-    .replace('YY', `${date.getFullYear() % 100}`)
-    .replace('MMMM', `${config.monthNames[date.getMonth()]}`)
-    .replace('MMM', `${config.monthNamesShort[date.getMonth()]}`)
-    .replace('MM', padStart(`${date.getMonth() + 1}`, 2, '0'))
-    .replace('M', `${date.getMonth() + 1}`)
-    .replace('DD', padStart(`${date.getDate()}`, 2, '0'))
-    .replace('D', `${date.getDate()}`)
-    .replace('HH', padStart(`${date.getHours()}`, 2, '0'))
-    .replace('H', `${date.getHours()}`)
-    .replace('hh', padStart(`${date.getHours() % 12}`, 2, '0'))
-    .replace('h', `${date.getHours() % 12}`)
-    .replace('mm', padStart(`${date.getMinutes()}`, 2, '0'))
-    .replace('m', `${date.getMinutes()}`)
-    .replace('ss', padStart(`${date.getSeconds()}`, 2, '0'))
-    .replace('s', `${date.getSeconds()}`)
-    .replace('A', date.getHours() < 12 ? 'AM' : 'PM')
-    .replace('a', date.getHours() < 12 ? 'am' : 'pm');
+    // Normalize tokens
+    .replace('YYYY', '%01%')
+    .replace('YY', '%02%')
+    .replace('MMMM', '%03%')
+    .replace('MMM', '%04%')
+    .replace('MM', '%05%')
+    .replace('M', '%06%')
+    .replace('DD', '%07%')
+    .replace('D', '%08%')
+    .replace('HH', '%09%')
+    .replace('H', '%10%')
+    .replace('hh', '%11%')
+    .replace('h', '%12%')
+    .replace('mm', '%13%')
+    .replace('ss', '%14%')
+    .replace('s', '%15%')
+    .replace('A', '%16%')
+    .replace('a', '%17%')
+    // Insert values
+    .replace('%01%', padZeros(date.getFullYear(), 4))
+    .replace('%02%', padZeros(date.getFullYear() % 100, 2))
+    .replace('%03%', `${config.monthNames[date.getMonth()]}`)
+    .replace('%04%', `${config.monthNamesShort[date.getMonth()]}`)
+    .replace('%05%', padZeros(date.getMonth() + 1, 2))
+    .replace('%06%', `${date.getMonth() + 1}`)
+    .replace('%07%', padZeros(date.getDate(), 2))
+    .replace('%08%', `${date.getDate()}`)
+    .replace('%09%', padZeros(date.getHours(), 2))
+    .replace('%10%', `${date.getHours()}`)
+    .replace('%11%', padZeros(date.getHours() % 12, 2))
+    .replace('%12%', `${date.getHours() % 12}`)
+    .replace('%13%', padZeros(date.getMinutes(), 2))
+    .replace('%14%', padZeros(date.getSeconds(), 2))
+    .replace('%15%', `${date.getSeconds()}`)
+    .replace('%16%', date.getHours() < 12 ? 'AM' : 'PM')
+    .replace('%17%', date.getHours() < 12 ? 'am' : 'pm');
 };
