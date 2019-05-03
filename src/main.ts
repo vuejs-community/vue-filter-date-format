5d4cc67acd9ec6a1c62def53af90bc17cac07e01
Emanuel <emanuelgianico@gmail.com>
2019-05-03 13:08:16 -0300

    Check if input is type Date
    If is not, returns empty string.

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ export { dateFormat } from './filters/dateFormat';
 export default {
   install(Vue: VueConstructor, baseConfig: Partial<IDateFormatConfig>): void {
     Vue.filter('dateFormat', (date: Date, format: string, config: Partial<IDateFormatConfig> = {}) => {
+      if (!(date instanceof Date)) return "";
       return dateFormat(date, format, { ...config, ...baseConfig });
     });
   },
